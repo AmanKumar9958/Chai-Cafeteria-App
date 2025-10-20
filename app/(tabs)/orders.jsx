a4e7f124e658633073e69bc5d67ba8fb6497d07b
Aman Kumar <151604983+AmanKumar9958@users.noreply.github.com>
2025-10-20 23:15:30 +0530

    - Updated order page

--- app/(tabs)/orders.jsx
+++ app/(tabs)/orders.jsx
@@ -4,7 +4,7 @@ import { View, Text, SafeAreaView } from 'react-native';
 export default function OrdersScreen() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center">
-      <Text className="text-2xl">Orders Screen</Text>
+      <Text className="text-2xl">Your All order will appear here</Text>
     </SafeAreaView>
   );
 }
